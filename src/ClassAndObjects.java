dadd29e61551f18c26ff9785102a35aa475e41b7
Nihal Sheikh <nihalsheikh585@gmail.com>
2024-11-13 14:52:41 +0530

    Code update

---
+++ src/ClassAndObjects.java
@@ -0,0 +1,42 @@
+import org.w3c.dom.ls.LSOutput;
+
+class Main2 {
+    int x = 10;
+}
+
+public class ClassAndObjects {
+    int a = 11;
+
+    // Method
+    static void meth1() {
+        String greet = "Hii there!";
+        System.out.println(greet);
+    }
+
+    int meth1(int x, int y) {
+        int sum = x + y;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        // Creating and object
+        ClassAndObjects myObject = new ClassAndObjects();
+
+        // calling the object
+        System.out.println(myObject.a);
+
+        // Calling class
+        Main2 myClassObj = new Main2();
+        System.out.println(myClassObj.x);
+
+        // calling method
+        // direct calling the static method
+        meth1();
+
+        // making object to call method
+        ClassAndObjects myObject2 = new ClassAndObjects();
+        System.out.println(myObject2.meth1(30, 6));
+
+    }
+}
+
---
+++ src/DataTypesAndVariables.java
@@ -0,0 +1,41 @@
+public class DataTypesAndVariables {
+    public static void main(String[] args) {
+        // Variables : where values can be stored.
+
+        // Primitive Datatypes and Declaring variables
+        byte b; // b is a variable of type byte.
+        short s; // s is a variable of type short.
+        int i; // i is a variable of type int.
+        long l; // l is a variable of type long.
+        float f; // f is a variable of type float.
+        double d; // d is variable of type double.
+        boolean bool; // bool is a variable of type boolean.
+        char c; // c is a variable of type char.
+
+        // Initializing variables
+        b = 127;
+        s = 32767;
+        i = 20;
+        l = 2000000000;
+        f = 1.2345F;
+        d = 1.2345678999;
+        bool = true;
+        c = 'n';
+
+        // Declaring and Initializing in the same line
+        int a = 10;
+
+        // Multiple variables declaration in same line
+        int p, q, r; // use comma (,) to separate variables
+
+        // Declaration and Initialization multiple variables
+        int m = 10, n = 2, o = 5; // use comma (,) to separate variables
+
+        // Printing variables
+        System.out.println(m);
+
+        // Multiple Variable Printing
+        System.out.println(m + " " + n + " " + o); // use + to concatenate values
+        System.out.println(m+n); // + will add variable values
+    }
+}
