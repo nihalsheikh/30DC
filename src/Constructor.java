47ab8638c6c83821793ff45398b98ac5541f2e30
Nihal Sheikh <nihalsheikh585@gmail.com>
2024-11-13 22:21:39 +0530

    Constructor Code

--- src/Constructor.java
+++ src/Constructor.java
@@ -1,5 +1,14 @@
 public class Constructor {
+    String greet;
+
+    // Constructor
+    public Constructor() {
+        greet = "Hello";
+    }
+
     public static void main(String[] args) {
-        System.out.println("Constructor");
+        // Calling a constructor
+        Constructor myConst = new Constructor();
+        System.out.println(myConst.greet);
     }
 }
