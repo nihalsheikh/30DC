cc159f825f2ee8c08a2b21df184b60cbd427d610
Nihal Sheikh <nihalsheikh585@gmail.com>
2024-11-17 23:04:14 +0530

    Code Update

---
+++ src/methodAdd.java
@@ -0,0 +1,10 @@
+public class methodAdd {
+    static  String greet() {
+        String greetings = "Good Evening";
+        return greetings;
+    }
+
+    public static void main(String[] args) {
+        greet();
+    }
+}
