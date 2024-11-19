3fdf0b76ea14df40f2cd5661446ee69c591e3edb
Nihal Sheikh <nihalsheikh585@gmail.com>
2024-11-19 21:45:20 +0530

    Questions on Variable and Data Types

---
+++ src/apna/practice/problems/questionsOfVariableModules/finalTypeOfResult.java
@@ -0,0 +1,15 @@
+package apna.practice.problems.questionsOfVariableModules;
+
+public class finalTypeOfResult {
+    public static void main(String[] args) {
+        // Question 4: Type of final answer
+        byte b = 4;
+        char c = 'a';
+        short s = 512;
+        int i = 1000;
+        float f = 3.14f;
+        double d = 99.954;
+
+        System.out.println("result = " + ((f * b) + (i % c) - (d * s)));
+    }
+}
---
+++ src/apna/practice/problems/questionsOfVariableModules/varNameConvention.java
@@ -0,0 +1,10 @@
+package apna.practice.problems.questionsOfVariableModules;
+
+public class varNameConvention {
+    public static void main(String[] args) {
+        // Checking variable name convention
+        int $ = 10;
+
+        System.out.println($);
+    }
+}
