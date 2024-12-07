2f5838006a6feaf6fd65ad5bcaed5d5e754a7617
Nihal Sheikh <nihalsheikh585@gmail.com>
2024-12-07 11:59:14 +0530

    Advance Patterns practice

--- src/com/apna/AdvancePatterns/InvertedHalfPyramid.java
+++ src/com/apna/AdvancePatterns/InvertedHalfPyramid.java
@@ -4,15 +4,19 @@ import java.util.Scanner;
 public class InvertedHalfPyramid {
     // Right-sided pyramid
     public static void InvPyramid(int n) {
-        // we will need 3 loops to print inverted pyramid
-        for(int i=0; i<=n; i++) {
-            for(int j=0; j<n-i; j++) {
+        for(int i=0; i<=n; i++) { // Loops for total lines or total rows
+
+            // Space Loop
+            for(int j=0; j<n-i; j++) { // this loop prints spaces before stars
                 System.out.print(" ");
             }
-            for(int j=0; j<i;j++) {
+
+            // Stars (*) Loop or any other, you can print (#) too
+            for(int j=0; j<i; j++) { // this loop prints stars
                 System.out.print("*");
             }
-            System.out.println();
+
+            System.out.println(); // go to next line after finishing a row
         }
     }
 
