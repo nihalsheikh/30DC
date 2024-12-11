29af90eeab93927bf4b8e7cd9b9d5f4324156648
Nihal Sheikh <nihalsheikh585@gmail.com>
2024-12-11 13:58:35 +0530

    Advance Patterns practice

--- src/com/apna/AdvancePatterns/Butterfly.java
+++ src/com/apna/AdvancePatterns/Butterfly.java
@@ -3,9 +3,43 @@ import java.util.Scanner;
 
 public class Butterfly {
     public static void Triangle(int n) {
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n+n; j++) {
+        // Code divided in 2 parts: Upper and Lower half
+        // Every half need 3 different loops: for stars, space and stars again
 
+        // Upper Part
+        for(int i=1; i<=n; i++) { // loop for no. of lines
+            // stars = i
+            for(int j=1; j<=i; j++) {
+                System.out.print("* ");
+            }
+
+            // spaces = 2 * (n-i)
+            for(int j=1; j<=2*(n-i); j++) {
+                System.out.print("  ");
+            }
+
+            // stars = i
+            for(int j=1; j<=i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // Lower Part
+        for(int i=n; i>=1; i--) {
+            // stars = i
+            for(int j=1; j<=i; j++) {
+                System.out.print("* ");
+            }
+
+            // spaces = 2 * (n-i)
+            for(int j=1; j<=2*(n-i); j++) {
+                System.out.print("  ");
+            }
+
+            // stars = i
+            for(int j=1; j<=i; j++) {
+                System.out.print("* ");
             }
             System.out.println();
         }
