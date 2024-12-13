6c8b1073c36e0f9523626fa6ab0143d7b3005267
Nihal Sheikh <nihalsheikh585@gmail.com>
2024-12-13 22:53:49 +0530

    Arrays Started

---
+++ src/com/apna/Arrays/Introduction.java
@@ -0,0 +1,10 @@
+package com.apna.Arrays;
+
+public class Introduction {
+    public static void main(String[] args) {
+        // ARRAYS
+        // Definition: List of elements of the similar Datatype
+        //             stored in a contiguous memory location
+        System.out.println("Arrays");
+    }
+}
