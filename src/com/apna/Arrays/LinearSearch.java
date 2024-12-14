198b923119b33fc657005ebcee7fa31be276af15
Nihal Sheikh <nihalsheikh585@gmail.com>
2024-12-14 23:02:23 +0530

    Linear Search

--- src/com/apna/Arrays/LinearSearch.java
+++ src/com/apna/Arrays/LinearSearch.java
@@ -1,16 +1,20 @@
 package com.apna.Arrays;
 
 public class LinearSearch {
-    public static void main(String[] args) {
-        int[] arr = {2,4,6,8,10,12,14,16,18,20};
-
-        System.out.println("Element 10 is at position: ");
-
+    public static void Linear(int[] arr) {
         for(int i=0; i<arr.length; i++) {
             if(arr[i] == 10) {
                 System.out.print(i);
                 break;
             }
         }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2,4,6,8,10,12,14,16,18,20};
+
+        System.out.println("Element 10 is at position: ");
+
+        Linear(arr);
     }
 }
