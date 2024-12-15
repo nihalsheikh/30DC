069a186ffd2fcc3eaa9df459d93fc7ea02aabb08
Nihal Sheikh <nihalsheikh585@gmail.com>
2024-12-15 17:16:38 +0530

    Linear Search

--- src/com/apna/Arrays/LinearSearch.java
+++ src/com/apna/Arrays/LinearSearch.java
@@ -1,20 +1,50 @@
 package com.apna.Arrays;
 
 public class LinearSearch {
-    public static void Linear(int[] arr) {
+    // Linear Search: Searching an Array Element using key & finding index element
+    public static int LinearNum(int[] arr, int key) {
         for(int i=0; i<arr.length; i++) {
-            if(arr[i] == 10) {
-                System.out.print(i);
-                break;
+            if(arr[i] == key) {
+                return i;
             }
         }
+        return -1;
+    }
+
+    // Using String array to find a dish(i.e. key) in menu(i.e. array)
+    public static int StringMenu(String[] menu, String dish) {
+        for(int i=0; i<menu.length; i++) {
+            if(menu[i] == dish) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     public static void main(String[] args) {
-        int[] arr = {2,4,6,8,10,12,14,16,18,20};
+        int[] arr = {2,4,6,8,10,12,14,16,18,20}; // Number array
+        int key = 10;
 
-        System.out.println("Element 10 is at position: ");
+        String[] menu = {"Dosa", "Idli", "Poha", "Vadapav", "Upma", "Sabudana"};
+        String dish = "Vadapav";
 
-        Linear(arr);
+        int index = LinearNum(arr, key); // saving the return value of LinearNum method
+        int index2 = StringMenu(menu, dish); // saving the return value of StringMenu method
+
+        // Number Search
+        if(index == -1) {
+            System.out.println("Number not found");
+        }
+        else {
+            System.out.println("Element " + key + " is at index: " + index);
+        }
+
+        // Dish in the Menu
+        if(index2 == -1) {
+            System.out.println("Dish not found");
+        }
+        else {
+            System.out.println("Dish " + dish + " is at index: " + index2);
+        }
     }
 }
