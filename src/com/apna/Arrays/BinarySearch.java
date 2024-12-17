b5359dac4c5dffc51c73e06c3317a496deba3af9
Nihal Sheikh <nihalsheikh585@gmail.com>
2024-12-17 21:57:30 +0530

    Array: Binary Search

---
+++ src/com/apna/Arrays/BinarySearch.java
@@ -0,0 +1,41 @@
+package com.apna.Arrays;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class BinarySearch {
+    public static int Binary(int[] arr, int key) {
+        // Initializing the start and end index of array
+        int start = 0, end = arr.length - 1;
+
+        while(start <= end) { // condition will be true until we have a single element
+            int mid = (start + end) / 2; // calculate the mid-point of array
+
+            // Comparing the mid-element with key to find our key
+            if(arr[mid] == key) { // if mid is key, return its index num, which is mid
+                return mid; // when mid is found, return immediately
+            }
+            if (arr[mid] < key) { // & if arr[mid] is lesser, then search for key in right part of array
+                start = mid + 1; // update start
+            }
+            else { //  if arr[mid] is greater, search in the left part of our array
+                end = mid - 1; // updating end
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2,4,6,8,10,12,14};
+        int key = 14;
+
+        int BinSearch = Binary(arr, key);
+
+        if(BinSearch == -1) {
+            System.out.println("Element not found");
+        }
+        else {
+            System.out.println("Key "+key+" is ar index: "+BinSearch);
+        }
+    }
+}
--- src/com/apna/Arrays/LargestNum.java
+++ src/com/apna/Arrays/LargestNum.java
@@ -12,10 +12,23 @@ public class LargestNum {
         return largest; // method needs to return an int value
     }
 
+    public static int Smallest(int[] arr) {
+        int smallest = Integer.MAX_VALUE; // assigning biggest num(+infi) to smallest
+        for(int i=0; i<arr.length; i++) {
+            if(smallest > arr[i])
+                smallest = arr[i]; // updating smallest(+infi) with the small number
+        }
+
+        return smallest;
+    }
+
     public static void main(String[] args) {
         int[] arr = {1,3,63,5,7,9,11,70,12,16,50};
 
         int bigNum = Largest(arr); // strong the return value in bigNum variable
         System.out.println("Largest num is: " + bigNum);
+
+        int smallNum = Smallest(arr);
+        System.out.println("Smallest num is: " + smallNum);
     }
 }
