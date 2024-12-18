50282b8d7fb5552f97e4200c79ba9698869db7ce
Nihal Sheikh <nihalsheikh585@gmail.com>
2024-12-19 00:11:02 +0530

    Array: Pairs of an array

--- src/com/apna/Arrays/ArrayPairs.java
+++ src/com/apna/Arrays/ArrayPairs.java
@@ -1,5 +1,7 @@
 package com.apna.Arrays;
 
+import java.sql.SQLOutput;
+
 public class ArrayPairs {
     public static void Pairs(int[] arr) {
         for(int i=0; i<=arr.length-1; i++) { // outer loop for increasing i
@@ -10,9 +12,25 @@ public class ArrayPairs {
         }
     }
 
+    public static void Alt_Pairs(int[] arr) { // Alternate approach using a var
+        for(int i=0; i<arr.length-1; i++) {
+            int currentElement = arr[i];
+
+            for(int j=i+1; j<arr.length; j++) {
+                System.out.print("(" + currentElement + "," + arr[j] + ")" + " ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         int[] arr = {2,4,6,8,10};
 
+        System.out.println("Method 1");
         Pairs(arr);
+
+        System.out.println("Method 2");
+        Alt_Pairs(arr);
+
     }
 }
