8ad228bd4af4a5aab8ee4dad0a652c3ff937bda0
Nihal Sheikh <nihalsheikh585@gmail.com>
2024-12-22 15:31:30 +0530

    Array: Sum of sub-arrays and Total sub-arrays

--- src/com/apna/Arrays/MaxSubArrays.java
+++ src/com/apna/Arrays/MaxSubArrays.java
@@ -3,32 +3,48 @@ package com.apna.Arrays;
 public class MaxSubArrays {
     public static void Sub_Arrays(int[] arr) {
         int totalSubArrays = 0; // formula for counting total subArrays: (n*(n+1))/2
-        int currentSum = 0;
-        int maxSum = Integer.MIN_VALUE;
+        System.out.println("*************** Sub arrays start here ***************");
 
         for(int i=0; i<arr.length; i++) {
             int startElement = i;
 
             for(int j=i; j<arr.length; j++) { // not i+1 as we also want single subarray element
                 int endElement = j;
+
+                for(int k=startElement; k<=endElement; k++) {
+                    System.out.print(arr[k] + " "); // printing sub_arrays
+                }
+                totalSubArrays++; //in case of printing subarray un-comment this
+                System.out.println(); // print the next sub_array
+            }
+            System.out.println(); // next line
+        }
+    }
+
+    public static void SumOfSubArrays(int[] arr) {
+        int currentSum = 0; // calc sub array sum of that instant
+        int maxSum = Integer.MIN_VALUE;
+        int totalSubArrays = 0;
+
+        System.out.println("*************** Sum of Sub arrays start here ***************");
+
+        for(int i=0; i<arr.length; i++) {
+            int startElement = i;
+
+            for(int j=i+1; j<arr.length; j++) {
+                int endElement = j;
                 currentSum = 0;
 
                 for(int k=startElement; k<=endElement; k++) {
-//                    System.out.print(arr[k] + " "); // printing sub_arrays
-                    currentSum += arr[k]; // sum of sub Arrays
+                    currentSum += arr[k];
                 }
-//                totalSubArrays++; in case of printing subarray un-comment this
                 System.out.println(currentSum);
                 if(maxSum < currentSum) {
                     maxSum = currentSum;
                 }
-
-//                System.out.println(); // print the next sub_array
             }
-            System.out.println(); // next line
+            System.out.println();
         }
-//        int totalNumOfSubArrays = (arr.length*(arr.length + 1) )/ 2;
-//        System.out.println("Total Sub Arrays: " + totalSubArrays); // print in case of sub arrays
         System.out.println("Max sum of sub array = "+ maxSum);
     }
 
@@ -36,5 +52,6 @@ public class MaxSubArrays {
         int[] arr = {1,2,3,4,5};
 
         Sub_Arrays(arr);
+        SumOfSubArrays(arr);
     }
 }
---
+++ src/com/nareshIT/Test1_21Dec24.java
@@ -0,0 +1,17 @@
+package com.nareshIT;
+
+public class Test1_21Dec24 {
+    public static void main(String[] args) {
+        int x = 5;
+        boolean b1 = true;
+        boolean b2 = false;
+
+        if((x == 4) && !b2)
+            System.out.print("1 ");
+        System.out.println("2");
+
+        if((b2 == true) && b1)
+            System.out.print("3");
+
+    }
+}
