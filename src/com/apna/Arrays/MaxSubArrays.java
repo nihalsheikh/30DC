20ca686c56acbd5c44bca3913c7f0b084274a8d0
Nihal Sheikh <nihalsheikh585@gmail.com>
2024-12-22 13:27:02 +0530

    Array: Total Sub Arrays + Sum of Sub Arrays

--- src/com/apna/Arrays/MaxSubArrays.java
+++ src/com/apna/Arrays/MaxSubArrays.java
@@ -3,29 +3,37 @@ package com.apna.Arrays;
 public class MaxSubArrays {
     public static void Sub_Arrays(int[] arr) {
         int totalSubArrays = 0; // formula for counting total subArrays: (n*(n+1))/2
+        int currentSum = 0;
+        int maxSum = Integer.MIN_VALUE;
 
         for(int i=0; i<arr.length; i++) {
             int startElement = i;
 
             for(int j=i; j<arr.length; j++) { // not i+1 as we also want single subarray element
                 int endElement = j;
+                currentSum = 0;
 
                 for(int k=startElement; k<=endElement; k++) {
-                    System.out.print(arr[k] + " "); // printing sub_arrays
-//                    int sumSubArrays = 0;
-//                    System.out.print("sum of subarray = "+ (sumSubArrays+arr[k]) );
+//                    System.out.print(arr[k] + " "); // printing sub_arrays
+                    currentSum += arr[k]; // sum of sub Arrays
                 }
-                totalSubArrays++;
-                System.out.println(); // print the next sub_array
+//                totalSubArrays++; in case of printing subarray un-comment this
+                System.out.println(currentSum);
+                if(maxSum < currentSum) {
+                    maxSum = currentSum;
+                }
+
+//                System.out.println(); // print the next sub_array
             }
             System.out.println(); // next line
         }
 //        int totalNumOfSubArrays = (arr.length*(arr.length + 1) )/ 2;
-        System.out.println("Total Sub Arrays: " + totalSubArrays);
+//        System.out.println("Total Sub Arrays: " + totalSubArrays); // print in case of sub arrays
+        System.out.println("Max sum of sub array = "+ maxSum);
     }
 
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6};
+        int[] arr = {1,2,3,4,5};
 
         Sub_Arrays(arr);
     }
