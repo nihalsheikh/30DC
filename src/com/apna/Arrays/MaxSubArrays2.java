5f63bd1b400a9d51c1aba6ecee200eebf8e99d32
Nihal Sheikh <nihalsheikh585@gmail.com>
2024-12-24 23:39:37 +0530

    Array: Kadane's Algorithm

--- src/com/apna/Arrays/MaxSubArrays2.java
+++ src/com/apna/Arrays/MaxSubArrays2.java
@@ -8,23 +8,40 @@ public class MaxSubArrays2 {
     */
 
     public static void PrefixArraysSum(int[] arr) {
-        int currentSum = 0; // store the sum of subarray
-        int[] arr2 = new int[arr.length]; // store the sum of sub-array elemnt here increasing order of elements in sub-arrays
+        int[] prefix = new int[arr.length]; // store all sum of sub-array elements here increasing order of elements in sub-arrays
 
-        for(int i=0; i<arr.length; i++) {
-            arr2[i] = currentSum + arr[i];
-            currentSum = arr2[i];
+        // first element of prefix array will be same as original arr element
+        prefix[0] = arr[0];
+        for(int i=1; i<prefix.length; i++) { // starting from 1 as 0 is fixed
+            prefix[i] = prefix[i-1] + arr[i]; // calculating sum of sub-array formula
         }
 
-        System.out.println("Sum of Sub arrays is: ");
-        for(int i=0; i<arr2.length; i++) {
-            System.out.print(arr2[i] + " ");
+        System.out.println("Sum of Sub arrays is: "); // print the array containing sum
+        for(int i=0; i<prefix.length; i++) {
+            System.out.print(prefix[i] + " ");
         }
     }
 
-    public static void main(String[] args) {
-        int[] arr = {-1,2,4,6,8};
+    public static void Kadanes(int[] arr) {
+        int maxSum = Integer.MIN_VALUE;
+        int currSum = 0;
 
-        PrefixArraysSum(arr);
+        for(int i=0; i<arr.length; i++) {
+            currSum = currSum + arr[i];
+
+            if(currSum < 0)
+                currSum = 0;
+
+            maxSum = Math.max(currSum, maxSum);
+        }
+
+        System.out.println("Max sub-array sum = " + maxSum);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {-2, -1, 4, 3, 7};
+
+//        PrefixArraysSum(arr);
+        Kadanes(arr);
     }
 }
