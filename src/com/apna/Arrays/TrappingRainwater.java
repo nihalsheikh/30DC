2caab4618be8a361d5b15fc9813d4e060264a544
Nihal Sheikh <nihalsheikh585@gmail.com>
2024-12-28 22:42:24 +0530

    Array: Trapping Rainwater

--- src/com/apna/Arrays/TrappingRainwater.java
+++ src/com/apna/Arrays/TrappingRainwater.java
@@ -1,7 +1,14 @@
 package com.apna.Arrays;
 
 public class TrappingRainwater {
+    public static void Rainwater(int[] arr) {
+        // Trapping Rainwater logic
+    }
+
     public static void main(String[] args) {
         System.out.println("Trapping Rainwater");
+        int[] arr = {4,2,0,6,3,2,5};
+
+        Rainwater(arr);
     }
 }
